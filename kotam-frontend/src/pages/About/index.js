55b67147c0b851678e6a99e6820e1b36d498d918
Aiman Farhan <aimanfaruk98@gmail.com>
2021-12-07 23:59:50 +0800

    added semantic ui css

--- kotam-frontend/src/pages/About/index.js
+++ kotam-frontend/src/pages/About/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Link from 'react-router-dom';
-
+import "./style.css"
 
 const AboutPage = () => {
     return (
         <div>
-            <h1>Welcome to Kotam League!</h1>
+            <h1 class="welcomeBanner">Welcome to Kotam League!</h1>
         </div>
     );
 }
