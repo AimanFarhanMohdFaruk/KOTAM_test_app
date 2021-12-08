a0b28c71aacaafc2cde7a05dde4a541b93c792af
Aiman Farhan <aimanfaruk98@gmail.com>
2021-12-08 23:25:25 +0800

    added sidebar

--- kotam-frontend/src/components/Navbar/Navbar.js
+++ kotam-frontend/src/components/Navbar/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = () => {
     return (
     <div class="sidebar">
         <h2>Kotam</h2>
-        <a href="#home">About</a>
-        <a href="#signUp" class="active" >Sign Up</a>
+        <a href="#home" class="active">About</a>
+        <a href="#signUp"  >Sign Up</a>
         <a href="#standings">Standings</a>
-    </div>    
+    </div>
     );
 }
 
