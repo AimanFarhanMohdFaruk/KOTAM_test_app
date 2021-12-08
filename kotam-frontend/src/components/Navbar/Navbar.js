e0936ebaf31d9fd7ff4b3736d2b8fa4ec0da35ab
Aiman Farhan <aimanfaruk98@gmail.com>
2021-12-09 00:13:21 +0800

    added content page css

--- kotam-frontend/src/components/Navbar/Navbar.js
+++ kotam-frontend/src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './style.css'
+import tennis from "../../assets/images/tennis.png"
 
 
 const Navbar = () => {
 
     return (
     <div class="sidebar">
-        <h2>Kotam</h2>
-        <a href="#home" class="active">About</a>
+        <h2 >Kotam <img src={tennis} class="logo" alt="tennisBall"/></h2>
+        <a href="/about" class="active">About</a>
         <a href="#signUp"  >Sign Up</a>
         <a href="#standings">Standings</a>
     </div>
