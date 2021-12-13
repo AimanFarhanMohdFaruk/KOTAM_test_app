2b0994388a8c8ca4ace7dc64fe4b0aa515f57a50
vnonCodes <engageanon@gmail.com>
2021-12-13 20:34:52 +0800

    Update User.js
    Method for setting user passwords

--- backend/models/User.js
+++ backend/models/User.js
@@ -12,5 +12,9 @@ var UserSchema = new mongoose.Schema({
 }, {timestamps: true});
 
 UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+UserSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  };
 
 mongoose.model('User', UserSchema);
