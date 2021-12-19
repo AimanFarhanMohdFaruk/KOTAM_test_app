0ccd95f291976e67d4c80559ba6a83812c39aa67
vnonCodes <engageanon@gmail.com>
2021-12-19 23:20:16 +0800

    Update index.js
    removed secret

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://test-admin:spacebar123@cluster0.yjpt8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster0.yjpt8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 80;
 
 Mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
